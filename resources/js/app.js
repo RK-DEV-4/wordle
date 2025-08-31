c97f7f4e485f20ee70faa053bab3ada874403c66
Richard Knight <richardknightdevelopment@gmail.com>
2025-08-31 15:59:30 -0700

    Update package versions and resolve front end routes for inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import { createInertiaApp } from "@inertiajs/svelte";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import '../css/app.css';
+
+if (document.getElementById("app")) {
+    createInertiaApp({
+        title: "Wordle",
+        resolve: name =>
+            resolvePageComponent(
+                `./Pages/${name}.svelte`,
+                import.meta.glob('./Pages/**/*.svelte')
+            ),
+        setup({ el, App, props }) {
+            new App({ target: el, props });
+        },
+    });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/view/**/*.blade.php",
+    "./resources/js/**/*.svelte"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
